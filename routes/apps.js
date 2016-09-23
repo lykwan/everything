d3000c4fdf7afeac5bf0ca73a6e100acb2e8b87f
Lily Kwan <Lily@LaiYees-MacBook.local>
2016-09-23 10:51:21 -0700

    commented out apps routes

--- routes/apps.js
+++ routes/apps.js
@@ -2,29 +2,29 @@ const models = require('../models/index');
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', function(req, res) {
-  models.Apps.findAll()
-    .then(function(apps) {
-      res.send(apps);
-    }
-  });
-
-
-
-
-    if (user) {
-      res.json(user);
-    } else {
-      models.User.create({
-        fbId: req.session.userProfile.id,
-        name: req.session.userProfile.name
-      }).then(function(newUser) {
-        res.json(newUser);
-      }).catch(function(err) {
-        res.status(422).json({ error: err });
-      });
-    }
-  });
-});
+// router.get('/', function(req, res) {
+//   models.Apps.findAll()
+//     .then(function(apps) {
+//       res.send(apps);
+//     }
+//   });
+//
+//
+//
+//
+//     if (user) {
+//       res.json(user);
+//     } else {
+//       models.User.create({
+//         fbId: req.session.userProfile.id,
+//         name: req.session.userProfile.name
+//       }).then(function(newUser) {
+//         res.json(newUser);
+//       }).catch(function(err) {
+//         res.status(422).json({ error: err });
+//       });
+//     }
+//   });
+// });
 
 module.exports = router;
