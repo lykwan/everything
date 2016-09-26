2623621766409db8f4e010ff0b9f83b1ac8454d3
Lily Kwan <Lily@LaiYees-MacBook.local>
2016-09-26 10:26:03 -0700

    fixed the require files for index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.json')[env];
 const sequelize =
   new Sequelize(config.database, config.username, config.password, config);
-const SequelizeFixtures = require('sequelize-fixtures');
 const db = {};
 
 // add db to the sequelize
