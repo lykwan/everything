8bb3c20054c00042abf7712f61b4b42abbfd4e86
Lily Kwan <Lily@LaiYees-MacBook.local>
2016-09-29 10:04:05 -0700

    changed global subfeedplugins to app.locals

--- routes/subfeeds.js
+++ routes/subfeeds.js
@@ -2,44 +2,123 @@ const models = require('../models/index');
 const express = require('express');
 const router  = express.Router();
 
-router.post('/', function(req, res) {
-  models.Feed.findOrCreate({
-    where: {
-      pluginId: req.body.pluginId,
-      userId: req.session.user.id
-    }
-  }).spread(feed => {
-    console.log(feed.id);
-    return models.Subfeed.create({
-      feedId: feed.id,
-      name: req.body.subfeedName,
-      params: req.body.subfeedParams
+const itemsPerPage = 3;
+
+module.exports = function(app, client) {
+  router.post('/', function(req, res) {
+    models.Feed.findOrCreate({
+      where: {
+        pluginId: req.body.pluginId,
+        userId: req.session.user.id
+      }
+    }).spread(feed => {
+      console.log(feed.id);
+      return models.Subfeed.create({
+        feedId: feed.id,
+        name: req.body.subfeedName,
+        params: req.body.subfeedParams
+      });
+    }).then(subfeed => {
+      return models.Subfeed.find({
+        where: { id: subfeed.id },
+        include: [
+          { model: models.Feed, attributes: ['pluginId'], include: [
+            { model: models.Plugin, attributes: ['path'] }
+          ]}
+        ]
+      });
+    }).then(subfeed => {
+      const plugin = subfeed.Feed.Plugin;
+      const SubfeedPlugin = require(`../plugins/${ plugin.path }/backend.js`);
+      const pluginInstance = new SubfeedPlugin(subfeed.params);
+      app.locals.subfeedPlugins[subfeed.id] = pluginInstance;
+      res.json(subfeed);
     });
-  }).then(subfeed => {
-    return models.Subfeed.find({
-      where: { id: subfeed.id },
-      include: [
-        { model: models.Feed, attributes: ['pluginId'], include: [
-          { model: models.Plugin, attributes: ['path'] }
-        ]}
-      ]
-    });
-  }).then(subfeed => {
-    const plugin = subfeed.Feed.Plugin;
-    const SubfeedPlugin = require(`../plugins/${ plugin.path }/backend.js`);
-    const pluginInstance = new SubfeedPlugin(subfeed.params);
-    console.log(pluginInstance.initAuth);
-    console.log(pluginInstance);
-    req.session.subfeedPlugins[subfeed.id] = pluginInstance;
-    console.log(req.session.subfeedPlugins[subfeed.id]);
-    console.log(req.session.subfeedPlugins[subfeed.id].initAuth);
-    res.json(subfeed);
   });
-});
 
-// router.get('/authForm', function (req, res) {
-// get the auth form string, pass it to frontend??
+  // router.get('/authForm', function (req, res) {
+  // get the auth form string, pass it to frontend??
 
-// router.post('/:id/login', ) {
+  // router.post('/:id/login', ) {
 
-module.exports = router;
+
+  router.get('/:id', function(req, res) {
+    const subfeedPlugin = app.locals.subfeedPlugins[req.params.id];
+    let startRange, endRange;
+    client.hgetall(req.params.id, function(err, itemsObj) {
+      let feedItems = [];
+      console.log('itemsObj', itemsObj);
+      if (itemsObj) {
+        const min = Math.min.apply(Math, Object.keys(itemsObj));
+        const max = Math.max.apply(Math, Object.keys(itemsObj));
+        startRange = itemsPerPage * parseInt(req.query.page) + min;
+        endRange = itemsPerPage * (parseInt(req.query.page) + 1) + min;
+        console.log('page', req.query.page);
+        console.log('min', min);
+        console.log('startRange', startRange);
+        console.log('endRange', endRange);
+        if (itemsObj[endRange - 1] !== undefined) {
+          console.log('in this if block');
+          for (let i = startRange; i < endRange; i++) {
+            const feedItem = JSON.parse(itemsObj[i]);
+            feedItems.push(feedItem);
+          }
+          res.send({ feedItems: feedItems });
+        } else {
+          console.log('in this else block, fetching more data');
+          fetchSubfeedData(startRange,
+                            endRange,
+                            itemsObj,
+                            subfeedPlugin,
+                            req.params.id,
+                            max+1,
+                            res
+                          );
+        }
+      } else {
+        console.log('in the else block, fetching completely new data');
+        startRange = itemsPerPage * req.query.page;
+        endRange = itemsPerPage * (req.query.page + 1);
+        fetchSubfeedData(startRange,
+                          endRange,
+                          {},
+                          subfeedPlugin,
+                          req.params.id,
+                          0,
+                          res
+                        );
+      }
+    });
+  });
+
+  function fetchSubfeedData
+    (startRange, endRange, itemsObj, subfeedPlugin, subfeedId, startIdx, res) {
+    let feedItems = [];
+    // subfeedPlugin.getOlderData(itemsPerPage, dataPoints => {
+    const dataPoints = [{ title: 'hey1', img: 'blah' }, { title: 'what2', img: 'blah again' }, { title: 'whattttt3', img: 'blah againaayy' }, { title: 'whaaaaat4', img: 'yuppp again' }];
+      for (let i = 0; i < dataPoints.length; i++) {
+        const feedItem = dataPoints[i];
+        feedItems.push(feedItem);
+        itemsObj[startIdx + i] = JSON.stringify(feedItem);
+      }
+    // });
+    client.hmset(subfeedId, itemsObj, function(setErr, reply) {
+      if (reply === "OK") {
+        client.hgetall(subfeedId, function(getErr, fetchedItemsObj) {
+          let fetchedFeedItems = [];
+          for (let i = startRange; i < endRange; i++) {
+            const feedItem = JSON.parse(fetchedItemsObj[i]);
+            fetchedFeedItems.push(feedItem);
+          }
+
+          res.send({ feedItems: fetchedFeedItems });
+          return feedItems;
+        });
+      }
+    });
+  }
+
+
+  return router;
+
+};
--- server.js
+++ server.js
@@ -19,8 +19,6 @@ client.on('connect', function() {
   console.log('connected');
 });
 
-const itemsPerPage = 3;
-
 app.use(express.static('public'));
 
 // req body
@@ -40,6 +38,8 @@ let files = fs.readdirSync(path.join(__dirname, 'plugins'));
 models.Plugin.addNewPlugins(files);
 models.Plugin.cleanUpOldPlugins(files);
 
+
+
 // var findDocuments = function(db) {
 //   return new Promise(function (resolve, reject) {
 //     // Get the documents collection
@@ -58,7 +58,8 @@ models.Plugin.cleanUpOldPlugins(files);
 //   }).error(function
 // }
 
-let subfeedPlugins = {};
+// keeping all the subfeed plugin instances in app.locals
+app.locals.subfeedPlugins = {};
 
 app.post('/login', function(req, res) {
   https.get(
@@ -82,9 +83,8 @@ app.post('/login', function(req, res) {
             feeds, models.Feed, models.Plugin
           );
         }).then(subfeeds => {
-          // req.session.subfeedPlugins = {};
           subfeeds.forEach(subfeed => {
-            subfeed.createNewSubfeedPlugin(subfeedPlugins);
+            subfeed.createNewSubfeedPlugin(app.locals.subfeedPlugins);
           });
 
           res.send(req.session.user);
@@ -113,85 +113,7 @@ app.use(restrictLogin);
 app.use('/users', userRoutes);
 app.use('/plugins', pluginRoutes);
 app.use('/feeds', feedRoutes);
-app.use('/subfeeds', subfeedRoutes);
-
-app.get('/subfeeds/:id', function(req, res) {
-  const subfeedPlugin = subfeedPlugins[req.params.id];
-  let startRange, endRange;
-  client.hgetall(req.params.id, function(err, itemsObj) {
-    let feedItems = [];
-    console.log('itemsObj', itemsObj);
-    if (itemsObj) {
-      const min = Math.min.apply(Math, Object.keys(itemsObj));
-      const max = Math.max.apply(Math, Object.keys(itemsObj));
-      startRange = itemsPerPage * parseInt(req.query.page) + min;
-      endRange = itemsPerPage * (parseInt(req.query.page) + 1) + min;
-      console.log('page', req.query.page);
-      console.log('min', min);
-      console.log('startRange', startRange);
-      console.log('endRange', endRange);
-      if (itemsObj[endRange - 1] !== undefined) {
-        console.log('in this if block');
-        for (let i = startRange; i < endRange; i++) {
-          const feedItem = JSON.parse(itemsObj[i]);
-          feedItems.push(feedItem);
-        }
-        res.send({ feedItems: feedItems });
-      } else {
-        console.log('in this else block, fetching more data');
-        fetchSubfeedData(startRange,
-                          endRange,
-                          itemsObj,
-                          subfeedPlugin,
-                          req.params.id,
-                          max+1,
-                          res
-                        );
-      }
-    } else {
-      console.log('in the else block, fetching completely new data');
-      startRange = itemsPerPage * req.query.page;
-      endRange = itemsPerPage * (req.query.page + 1);
-      fetchSubfeedData(startRange,
-                        endRange,
-                        {},
-                        subfeedPlugin,
-                        req.params.id,
-                        0,
-                        res
-                      );
-    }
-  });
-});
-
-function fetchSubfeedData
-  (startRange, endRange, itemsObj, subfeedPlugin, subfeedId, startIdx, res) {
-  let feedItems = [];
-  // subfeedPlugin.getOlderData(itemsPerPage, dataPoints => {
-  const dataPoints = [{ title: 'hey1', img: 'blah' }, { title: 'what2', img: 'blah again' }, { title: 'whattttt3', img: 'blah againaayy' }, { title: 'whaaaaat4', img: 'yuppp again' }];
-    for (let i = 0; i < dataPoints.length; i++) {
-      const feedItem = dataPoints[i];
-      feedItems.push(feedItem);
-      itemsObj[startIdx + i] = JSON.stringify(feedItem);
-    }
-  // });
-  client.hmset(subfeedId, itemsObj, function(setErr, reply) {
-    if (reply === "OK") {
-      client.hgetall(subfeedId, function(getErr, fetchedItemsObj) {
-        let fetchedFeedItems = [];
-        for (let i = startRange; i < endRange; i++) {
-          const feedItem = JSON.parse(fetchedItemsObj[i]);
-          fetchedFeedItems.push(feedItem);
-        }
-
-        res.send({ feedItems: fetchedFeedItems });
-        return feedItems;
-      });
-    }
-  });
-}
-
-
+app.use('/subfeeds', subfeedRoutes(app, client));
 
 app.listen(3000, function () {
   console.log('Everything listening on port 3000!');
